49ac734b1a39bc03585090ea5ac92497359bcaa6
kangpangpang <kangpangpang@gmail.com>
2013-07-10 00:32:58 +0800

    U: update

--- bookmarklet/main.js
+++ bookmarklet/main.js
@@ -13,38 +13,79 @@
     graph.style.margin= '150px'
   }
 
+
   seajs.use('https://raw.github.com/leoner/seajs-health/master/bookmarklet/joint.all.min.js', function() {
 
     dimension(800, 400);
-
     var uml = Joint.dia.uml;
-    var paper = Joint.paper("dependencies_graph", 800, 900);
+    var paper = Joint.paper("dependencies_graph", 800, 400);
 
-    var s1 = uml.State.create({
-      rect: {x: 100, y: 50, width: 100, height: 60},
-      label: "state 1",
-      attrs: {
-        fill: "90-#000-green:1-#fff"
-      },
-      actions: {
-        method: "show()",
-        attr: "id"
+    var rx = 20
+    var ry = 20
+    var lx = 120
+    var ly = 20
+    var width = 100
+    var height = 60
+
+    function getRect(isRoot) {
+        if (isRoot) {
+            return {
+                x: rx,
+                y: ry+=150,
+                width: width,
+                height: height
+            }
+        } else {
+            return {
+                x: lx,
+                y: ly+=150,
+                width: width,
+                height: height
+            }
+        }
+    }
+
+    function isRoot(id) {
+      return /_use_\d+$/.test(id)
+    }
+
+    function createNode(id, methods, attrs) {
+
+        var opts = {
+            rect: getRect(isRoot(id))
+            attrs: {
+              fill: "90-#000-green:1-#fff"
+            },
+            actions: {}
+        }
+
+        opts.label = id
+
+        return uml.State.create(opts)
+    }
+
+    function findRoots() {
+      var roots = []
+      for (var key in seajs.cache) {
+        if (isRoot(key)) {
+          roots.push(key)
+        }
       }
-    })
+      return roots
+    }
 
-    var s2 = uml.State.create({
-      rect: {x: 290, y: 50, width: 100, height: 60},
-      label: "state 2",
-      attrs: {
-        fill: "90-#000-red:1-#fff"
-      },
-      actions: {
-        method: "hide()",
-        attr: "elem"
-      }
-    })
+    var roots = findRoots(), mod, node, deps
 
-    s1.joint(s2, uml.arrow)
+    for(var i = 0, len = roots.length; i < len; i++) {
+      mod = seajs.cache[roots[i]]
+      node = createNode(roots[i])
+      deps  = mod.dependencies
+
+      // Temp solution
+      deps.forEach(function(dep) {
+         mod.joint(createNode(dep), uml.arrow)
+      })
+    }
   })
 }())
 
